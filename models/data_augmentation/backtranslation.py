8e7f408e9695b2503c47d1e7fafab1c34bdee964
Gabriel de Olim Gaul <gd221@ic.ac.uk>
2025-02-27 03:25:27 +0000

    Improve synonym replacement by adding POS tags

--- models/data_augmentation/backtranslation.py
+++ models/data_augmentation/backtranslation.py
@@ -5,6 +5,7 @@ def backtranslate(text):
     back_translated = GoogleTranslator(source="japanese", target="en").translate(translated)
     return back_translated
 
-text = "It is miserable being poor and homeless in an affluent nation where one 's worth is measured by what one can afford to buy . Even those in the middle class with good and steady jobs struggle to keep their lifestyle ."
-print("Original:", text)
-print("Back Translated:", backtranslate(text))
+if __name__ == "__main__":
+    text = "It is miserable being poor and homeless in an affluent nation where one 's worth is measured by what one can afford to buy . Even those in the middle class with good and steady jobs struggle to keep their lifestyle ."
+    print("Original:", text)
+    print("Back Translated:", backtranslate(text))
--- models/data_augmentation/synonym_replacement.py
+++ models/data_augmentation/synonym_replacement.py
@@ -1,38 +1,97 @@
 import random
-from nltk.corpus import wordnet
+import nltk
+import pandas as pd
+from nltk.corpus import wordnet, stopwords
 
-def get_synonyms(word):
+STOPWORDS = stopwords.words("english")
+
+def get_wordnet_pos(tag):
+    if tag.startswith("J"):
+        return wordnet.ADJ
+    elif tag.startswith("V"):
+        return wordnet.VERB
+    elif tag.startswith("N"):
+        return wordnet.NOUN
+    elif tag.startswith("R"):
+        return wordnet.ADV
+    else:
+        return None
+
+def get_synonyms(word, pos):
     synonyms = set()
     
-    for syn in wordnet.synsets(word): 
-        for l in syn.lemmas(): 
-            synonym = l.name().replace("_", " ").replace("-", " ").lower()
-            synonym = "".join([char for char in synonym if char in ' qwertyuiopasdfghjklzxcvbnm'])
-            synonyms.add(synonym) 
-    
-    if word in synonyms:
-        synonyms.remove(word)
+    # Get synonyms from WordNet
+    for syn in wordnet.synsets(word):
+        # Only consider the synonyms of the same POS
+        if syn.pos() != pos:
+            continue
+        
+        for l in syn.lemmas():
+            # If the synonym is not the same as the word then add it to the list
+            if word.lower() != l.name().lower():
+                synonym = l.name().replace("_", " ").lower()
+                synonyms.add(synonym) 
     
     return list(synonyms)
 
-def synonym_replacement(text, num_words):
+def synonym_replacement(text, num_replacements):
     words = text.split()
+    new_words = words.copy()
+    # Get the POS tags for the words
+    pos_tags = nltk.pos_tag(words)
     
+    # If there are no words in the text then return the text
     if len(words) == 0:
         return text
     
-    words_to_replace = random.sample(words, min(num_words, len(words)))
+    # Shuffle the indices of the words
+    shuffled_indices = list(range(len(words)))
+    random.shuffle(shuffled_indices)
     
-    new_words = words.copy()
-    
-    for word in words_to_replace:
-        synonyms = get_synonyms(word)
+    words_replaced = []
+    word_pos_tags = []
+    replaced = 0
+    for i in shuffled_indices:
+        # If the word is a stopword then skip it
+        word = words[i]
+        if word in STOPWORDS:
+            continue
+        
+        # Get the POS tag of the word
+        tag = pos_tags[i][1]
+        wordnet_pos = get_wordnet_pos(tag)
+        # If the wordnet_pos is None then skip the word
+        if wordnet_pos is None:
+            continue
+        
+        # Otherwise, get the synonyms of the word
+        synonyms = get_synonyms(word, pos=wordnet_pos)
         if synonyms:
-            new_word = random.choice(synonyms)
-            new_words = [new_word if w == word else w for w in new_words]
+            # Replace the word with a random synonym
+            synonym = random.choice(synonyms)
+            new_words[i] = synonym
+            words_replaced.append((word, synonym))
+            word_pos_tags.append((word, tag))
+            replaced += 1
+        
+        # If the number of words replaced is equal to the number of words to replace then break
+        if replaced >= num_replacements:
+            break
     
-    return " ".join(new_words)
+    return " ".join(new_words), words_replaced, word_pos_tags
+
+def augment_dataset(dataset, num_replacements=3):
+    augmented_rows = []
+    for _, row in dataset.iterrows():
+        augmented_text, _, _ = synonym_replacement(row["text"], num_replacements)
+        augmented_rows.append({"text": augmented_text, "label": row["label"]})
+    
+    augmented_df = pd.DataFrame(augmented_rows)
+    return pd.concat([dataset, augmented_df], ignore_index=True)
     
 if __name__ == "__main__":
     text = "The quick brown fox jumps over the lazy dog"
-    print(synonym_replacement(text, 3))
+    new_sentence, words_replaced, word_pos_tags = synonym_replacement(text, 3)
+    print(new_sentence)
+    print(words_replaced)
+    print(word_pos_tags)
