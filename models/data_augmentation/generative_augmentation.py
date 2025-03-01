027ae517689446a036155e35ab3634ea2a88513e
Siya Gowda <sg2121@ic.ac.uk>
2025-03-01 15:29:29 +0000

    correct generative augmentation, doesnt work great but thats probably bc of the model

--- models/data_augmentation/generative_augmentation.py
+++ models/data_augmentation/generative_augmentation.py
@@ -3,9 +3,24 @@ from transformers import pipeline
 paraphrase_pipe = pipeline("text2text-generation",
                            model="ramsrigouthamg/t5_paraphraser")
 
+def paraphrase(text, max_length=150):
+    return paraphrase_pipe(f"Paraphrase: {text}", 
+                           max_length=max_length, 
+                           num_return_sequences=1, 
+                           do_sample=True,  # Enables more variation in output
+                           top_k=50,  # Controls randomness
+                           top_p=0.95  # Nucleus sampling
+                           )[0]["generated_text"]
 
-def paraphrase(text):
-    return paraphrase_pipe(f"Paraphrase: {text}")[0]["generated_text"]
 
+print()
+text = "this is a paraphrased sentence"
+print(text)
+print("after paraphrasing: ")
+print(paraphrase(text))
+print()
 
-print(paraphrase("this is a paraphrased sentence"))
+text = "It is miserable being poor and homeless in an affluent nation where one 's worth is measured by what one can afford to buy . Even those in the middle class with good and steady jobs struggle to keep their lifestyle ."
+print(text)
+print("after paraphrasing: ")
+print(paraphrase(text, max_length=len(text)))
